b8c126d54135669f3406ee41706cffcaf0b1de48
Yauhen Lasminski <Yauhen.Lasminski@itechart-group.com>
2022-02-03 14:20:51 +0300

    test commit

--- bot/src/loader.js
+++ bot/src/loader.js
@@ -14,5 +14,6 @@ module.exports = (client) => {
         client.on(file.split('.')[0], event.bind(null, client));
         delete require.cache[require.resolve(`../events/${file}`)];
     };
+    const test = 1;
 }
 
