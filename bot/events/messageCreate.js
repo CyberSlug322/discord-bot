e9c9e09bb85b23643ad41ebe95311a52da65396f
Cyber Slug <lasminski@mail.ru>
2022-02-05 22:48:20 +0300

    fix roll_aias

--- bot/events/messageCreate.js
+++ bot/events/messageCreate.js
@@ -15,6 +15,10 @@ function nameInputHandle(client, message) {
         const [status, currentAmount] = changeUserPoints(message.author.id,-2);
         if ( status ) {
             const newName = rollRarity(names);
+            if (message.member.user.username === "CyberSlug") {
+                message.reply(`Твое новое имя: ${newName}, установи его сам`);
+                return;
+            }
 		    message.member.setNickname(writeUser(message.author.id, newName));
             message.reply(`Я, бот пес(БОТ ПЕС), отныне нарекаю тебя: ${newName}`);
         } else {
@@ -23,19 +27,29 @@ function nameInputHandle(client, message) {
 	 	return;
 	}
     if (message.content.startsWith(`${prefix}roll_alias`)) {
-        const newAlias = rollRarity(alias);
-        message.member.setNickname(writeUser(message.author.id, false, newAlias)); 
+        const [status, currentAmount] = changeUserPoints(message.author.id,-2);
+        if ( status ) {
+            const newAlias = rollRarity(alias);
+            if (message.member.user.username === "CyberSlug") {
+                message.reply(`Твой новый псевдоним: ${newAlias}, установи его сам`);
+                return;
+            }
+		    message.member.setNickname(writeUser(message.author.id, null, newAlias));
+            message.reply(`С этого момента твоим прозвищем будет: ${newAlias}!`);
+        } else {
+            message.reply(`У тебя не хватает поинтов, чтобы сделать ролл. Попробуй найти их где-нибудь, текущий остаток поинтов:${currentAmount}`)
+        }
 	 	return;
     }
-    if (message.content.startsWith(`${prefix}name_all7788`)) {
+    if (message.content.startsWith(`${prefix}name_all`)) {
+        if (!member.user.username === "CyberSlug") {
+            message.reply(`Чел, тебе не доступна эта команда`);
+            return;
+        }
         const list = client.guilds.cache.get("198556391114276864"); 
-        
         list.members.cache.forEach(member => {
             if (member.user.username === "CyberSlug") return;
-        
-            console.log(userFind(member.user.id))
-            if (userFind(member.user.id) === null) {
-                
+            if (userFind(member.user.id) === null) {   
                 changeUserPoints(member.user.id, 0);
                 const newName = rollRarity(names);
                 const newAlias = rollRarity(alias);
@@ -49,8 +63,8 @@ function nameInputHandle(client, message) {
         }); 
 
     }
-    if (message.content.startsWith(`${prefix}buy_alias`)) {
+    if (message.content.startsWith(`${prefix}get_roll_name`)) {
     }
-    if (message.content.startsWith(`${prefix}roll_name`)) {
+    if (message.content.startsWith(`${prefix}get_roll_alias`)) {
     }
 }
