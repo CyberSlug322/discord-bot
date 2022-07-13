3d5ad100fe5bed29f50d35577e71cc7691bbd120
Vitovt <zika.zika2013@mail.ru>
2022-07-13 14:38:04 +0200

    update 13.07.2022

---
+++ config.js
@@ -0,0 +1,7 @@
+const token = "OTM0MDgwMjk4OTAyMjUzNjM5.Yeq4EQ.5l5rTrXKLSc6lL94IkFAznOc0Og"
+const prefix = "!"
+const clientId = "934080298902253639"
+const guildId  = "198556391114276864"
+const MONGO_URL = "mongodb+srv://admin:admin@cluster0.dptsl.mongodb.net/discordBot?retryWrites=true&w=majority"
+
+module.exports = { token, prefix, clientId, guildId, MONGO_URL }
