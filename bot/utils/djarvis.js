0ca2f4ebc3d04f96912463b9d6762a98f1b280c4
Vitovt555 <zika.zika2015@mail.ru>
2024-06-16 20:42:34 +0200

    update djrvis

--- bot/utils/djarvis.js
+++ bot/utils/djarvis.js
@@ -1,30 +1,31 @@
-// import { playerMusic } from "../messages/playerMusic.js"    
-// const KDTime = {}  
-// export const djarvis = async (client) => {
-//     try {
-//             await client.on('voiceStateUpdate', (s, n) => {
-//                 let id = n.id
-//                 console.log(id)
+import { playerMusic } from '../messages/playerMusic.js'
+const MembersTimer = {}
+export const djarvis = async (client) => {
+    try {
+        await client.on('voiceStateUpdate', (s, n) => {
+            let id = n.id
+            console.log(id)
 
-//                 if(Date.now() - KDTime[id] < 1000*60*60*8) {
-//                     return
-                    
-//                 }
+            if (Date.now() - MembersTimer[id] < 1000 * 60 * 60 * 48) {
+                return
+            }
 
-//                 KDTime[id] = Date.now()
+            MembersTimer[id] = Date.now()
 
+            console.log('play', MembersTimer)
+            if (n.channelId) {
+                let roll = Math.random()
+                if (id == '972207964771479553') return
+                if (roll >= 0.5) {
+                    playerMusic(n.channelId, 'https://www.youtube.com/watch?v=KpTHh_8nj9g&ab_channel=Crazy_nick')
 
-//                 console.log('play', KDTime)
-//                  if (n.channelId) {
-//                     if(id == '972207964771479553') return
+                    return
+                }
 
-//                  playerMusic(n.channelId, 'https://www.youtube.com/watch?v=LuuOpozKbvE&ab_channel=V%C3%A1clav')
-                 
-//                 }
-//             }) 
-               
-//     }
-//     catch (err) {
-//             console.log(err)
-//     }
-// }//
+                playerMusic(n.channelId, 'https://www.youtube.com/watch?v=8Bx647FZlYQ&ab_channel=SoundeffectsPro')
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
